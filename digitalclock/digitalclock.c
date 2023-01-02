80013c7f07fb6edfc5a952a2d0ba0e25f4d4d67c
Damitha Kalhara <iaddkalhara1@gmail.com>
2023-01-03 00:58:47 +0530

    added code

--- digitalclock/digitalclock.c
+++ digitalclock/digitalclock.c
@@ -4,9 +4,15 @@
 int main(void)
 {
     int h, m, s;
+    int delay = 1000;
 
     printf("Set time: ");
     scanf("%d%d%d", &h, &m, &s);
 
+    if (h > 12 || m > 60 || s > 60)
+    {
+        printf("ERROR !");
+        exit(0);
+    }
     
 }
