709e63b92471212924f2c92289e892db4eee7307
Damitha Kalhara <iaddkalhara1@gmail.com>
2023-01-03 01:42:24 +0530

    finished

--- digitalclock/digitalclock.c
+++ digitalclock/digitalclock.c
@@ -6,7 +6,7 @@ int main(void)
     int h, m, s;
     int delay = 1000;
 
-    printf("Set time: ");
+    printf("Set start time: \n");
     scanf("%d%d%d", &h, &m, &s);
 
     if (h > 12 || m > 60 || s > 60)
@@ -14,5 +14,34 @@ int main(void)
         printf("ERROR !");
         exit(0);
     }
-    
+
+    // Create infinite loop
+    while (TRUE)
+    {
+        s++;
+        if (s > 59)
+        {
+            m++;
+            s = 0;
+        }
+        if (m > 59)
+        {
+            h++;
+            m = 0;
+        }
+        if (h > 12)
+        {
+            h = 1;
+        }
+
+        printf("Clock:\n");
+        printf("%02d:%02d:%02d", h, m, s);
+
+        // Slowdown the while loop
+        Sleep(delay);
+
+        // Clear screen
+        system("cls");
+    }
+
 }
